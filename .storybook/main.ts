03ecc964535dd3f483b07f5459da184cf0e5915f
Hudson Holanda <hudsonholanda55@gmail.com>
2023-05-31 20:20:36 -0300

    ci: Add publish storybook workflow

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,6 +13,11 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
-  
+  viteFinal: (config, { configType }) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/ignite-lab-design-system/'
+    }
+    return config
+  }
 };
 export default config;
