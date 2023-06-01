5558f6f5428d2a03441a691382c1dfe04e4231ed
Hudson Holanda <hudsonholanda55@gmail.com>
2023-05-31 22:09:35 -0300

    feat: add className prop

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,11 +2,12 @@ export interface HeadingProps {
   size?: 'sm' | 'md' | 'lg';
   children: ReactNode;
   asChild?: ReactNode;
+  className?: string;
 }
 import { Slot } from '@radix-ui/react-slot'
 import { clsx } from 'clsx'
 import { ReactNode } from 'react';
-export function Heading({ size = 'md', children, asChild } : HeadingProps) {
+export function Heading({ size = 'md', children, asChild, className} : HeadingProps) {
   const Comp = asChild ? Slot : 'h2'
   return (
     <Comp className={
@@ -15,7 +16,8 @@ export function Heading({ size = 'md', children, asChild } : HeadingProps) {
         'text-lg': size === 'sm',
         'text-xl': size === 'md',
         'text-2xl': size === 'lg',
-      }
+      },
+      className
     )}
     >
       {children}
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -2,11 +2,12 @@ export interface TextProps {
   size?: 'sm' | 'md' | 'lg';
   children: ReactNode;
   asChild?: ReactNode;
+  className?: string
 }
 import { Slot } from '@radix-ui/react-slot'
 import { clsx } from 'clsx'
 import { ReactNode } from 'react';
-export function Text({ size = 'md', children, asChild } : TextProps) {
+export function Text({ size = 'md', children, asChild, className } : TextProps) {
   const Comp = asChild ? Slot : 'span'
   return (
     <Comp className={
@@ -15,7 +16,8 @@ export function Text({ size = 'md', children, asChild } : TextProps) {
         'text-xs': size === 'sm',
         'text-sm': size === 'md',
         'text-md': size === 'lg',
-      }
+      },
+      className
     )}
     >
       {children}
